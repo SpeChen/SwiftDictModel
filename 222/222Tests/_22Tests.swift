202376a832d7ce2ba4c80e1605344d9b6647df2d
coderCSF <csf62615@sina.com>
2015-03-15 17:12:36 +0800

    ttttt

---
+++ 222/222Tests/_22Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _22Tests.swift
+//  222Tests
+//
+//  Created by coderCSF on 15/3/15.
+//  Copyright (c) 2015年 coderCSF. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class _22Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
